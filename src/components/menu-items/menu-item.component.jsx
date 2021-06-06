3991b00859d577f23404e170a8c10c6191d10d26
lovedeep <08lovedeep@gmail.com>
2021-06-06 14:47:07 +0530

    update our menu-items to navigate withRouter

--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -16,5 +20,5 @@ const MenuItem = ({ title, imageUrl, size }) => (
     </div>
   </div>
 );
-
-export default MenuItem;
+// here we added 2 divs inside because we dont want to scale content we want only background
+export default withRouter(MenuItem);
